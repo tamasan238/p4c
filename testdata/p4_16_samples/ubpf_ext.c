b5fb0f820d85671a3f1c6c1ee2f2f6c05901ad2f
Masaki I. <m1117@aol.jp>
2025-11-13 21:15:24 +0900

    update

--- testdata/p4_16_samples/ubpf_ext.c
+++ testdata/p4_16_samples/ubpf_ext.c
@@ -2,6 +2,8 @@
 #include "ubpf_ext.h"
 
 // #include <syslog.h>
+// 外部関数ではsyslog呼べない．
+// Failed to load code: bad R_BPF_64_64 relocation section って言われる．
 
 uint64_t get_vm_info(){
 	// syslog(LOG_INFO, "@@@ get_vm_info() is called. @@@");
